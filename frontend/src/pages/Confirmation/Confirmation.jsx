23b572dca675501e7e2d673946a99bb532f5de84
Kuba <272264@student.pwr.edu.pl>
2025-05-11 22:26:15 +0200

    Added confirmation page

--- frontend/src/pages/Confirmation/Confirmation.jsx
+++ frontend/src/pages/Confirmation/Confirmation.jsx
@@ -1,47 +1,61 @@
-import React from 'react';
+import React from "react";
 import "./confirmation.css";
-import assets from '../../assets/assets';
+import assets from "../../assets/assets";
 import { FaStar } from "react-icons/fa";
+import { IoIosStar } from "react-icons/io";
+import { MdOutlineStar } from "react-icons/md";
 
 const Confirmation = () => {
-    return (
-        <div className="confirmation">
-            <div className="confirmationContainer">
-                <div className="confirmationContent">
-                    {/* Icon */}
-                    <div className="confirmationIcon">
-                        <FaStar className="confirmationContentIcon" />
-                        <FaStar className="confirmationContentIcon" />
-                        <p className="confirmationIconText">
-                            Qitchen
-                        </p>
-                        <FaStar className='confirmationContentIcon' />
-                        <FaStar className='confirmationContentIcon' />
-                    </div>
+  return (
+    <div className="confirmation">
+      <div className="confirmationContainer">
+        <div className="confirmationContainerDiv">
+          <div className="confirmationContent">
+            <div className="confirmationContentContainer">
+              <div className="confirmationContentContainerDiv">
+                {/* Icon */}
+                <div className="confirmationIcon">
+                  <div className="confirmationIconOne">
+                    <MdOutlineStar className="confirmationContentIcon" />
+                    <MdOutlineStar className="confirmationContentIcon" />
+                  </div>
 
-                    {/* Title */}
-                    <div className="confirmationTitle">
-                        <h1 className="confirmationTitleText">Reservation Confirmed!</h1>
-                    </div>
+                  <div className="confirmationIconTwo">
+                    <p className="confirmationIconText">Qitchen</p>
+                  </div>
 
-                    {/* Message */}
-                    <div className="confirmationMessage">
-                        <p className="confirmationMessageText inter">
-                            Thank you for confirming your reservation at Qitchen. We look forward to serving you an exceptional dining experience!
-                        </p>
-                    </div>
-
-                    {/* Back to Home Button */}
-                    <div className="confirmationButton">
-                        <a href="/" className="confirmationButtonLink">
-                            <button className="confirmationButtonStyle">Back to Home</button>
-                        </a>
-                    </div>
+                  <div className="confirmationIconThree">
+                    <MdOutlineStar className="confirmationContentIcon" />
+                    <MdOutlineStar className="confirmationContentIcon" />
+                  </div>
                 </div>
+
+                {/* Title */}
+                <div className="confirmationTitle">
+                  <p className="confirmationTitleText">Reservation Confirmed!</p>
+                </div>
+
+                {/* Message */}
+                <div className="confirmationMessage">
+                  <p className="confirmationMessageText inter">
+                    Thank you for confirming your reservation at Qitchen. We look forward to serving you an exceptional dining experience!
+                  </p>
+                </div>
+
+                <div class="confirmationButton">
+                  <div class="confirmationButtonLink">
+                    <span>
+                      <p className="confirmationButtonLinkText inter">Back to Home</p>
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-// Ensure there is no <Router> here
 export default Confirmation;
