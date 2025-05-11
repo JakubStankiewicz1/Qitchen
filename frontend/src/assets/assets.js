61bbadef13c7a7b6a1d8a7644bffd84f37a01d85
Kuba <272264@student.pwr.edu.pl>
2025-05-11 14:27:06 +0200

    Fixed blog page

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -6,15 +6,12 @@ import gridIcon from "./Grid.png";
 import menuImage from "./menuImage.png";
 import menuTitleImgOne from "./menuTitleImgOne.png";
 import menuTitleImgTwo from "./menuTitleImgTwo.png";
-
 import volcanoDelight from "./volcanoDelight.png";
 import leafIcon from "./leafIcon.png";
-
 import spicyTunaMakiImage from "./spicyTunaMaki.png";
 import mangoMakiImage from "./mangoMaki.png";
 import salmonMakiImage from "./salmonMaki.png";
 import tunaMakiImage from "./tunaMaki.png";
-
 import volcanoDelightImage from "./volcanoDelight.png";
 import rainbowFusionImage from "./rainbowFusion.png";
 import dragonEleganceImage from "./dragonElegance.png";
@@ -22,50 +19,37 @@ import sunsetSerenityImage from "./sunsetSerenity.png";
 import mysticGardenImage from "./mysticGarden.png";
 import oceanBreezeImage from "./oceanBreeze.png";
 import tokyoBlossomImage from "./tokyoBlossom.png";
-
-
 import sunriseBlissImage from "./sunriseBliss.png";
 import mangoTangoFusionImage from "./mangoTangoFusion.png";
 import TruffleIndlugenceImage from "./truffleIndulgence.png";
 import pacificFirecrackerImage from "./pacificFirecracker.png";
 import EternalEllEnchantment from "./eternalEeelEnchantment.png";
-
 import backgroundGridImage from "./backgroundGrid.jpg";
-
 import diamondIcon from "./diamondIcon.png";
-
 import backgroundImage from "./backgroundImage.png";
-
 import aboutImageOne from "./aboutImageOne.png";
 import aboutImageTwo from "./aboutImageTwo.png";
 import aboutImageThree from "./aboutImageThree.png";
-
 import aboutStarIcon from "./aboutStarIcon.png";
-
 import leftArrow from "./leftArrow.png";
 import rightArrow from "./rightArrow.png";
-
 import contactImageOne from "./contactImageOne.png";
 import contactImageTwo from "./contactImageTwo.png";
 import contactImageThree from "./contactImageThree.png";
 import contactImageFour from "./contactImageFour.png";
-
 import contactImageFive from "./contactImageFive.png";
-
 import blogImageOne from "./blogImageOne.png";
-
-
-
 import blogImageTwo from "./blogImageTwo.png";
 import blogImageThree from "./blogImageThree.png";
 import blogImageFour from "./blogImageFour.png";
 import blogImageFive from "./blogImageFive.png";
 import blogImageSix from "./blogImageSix.png";
-
-
 import reservationImageOne from "./reservationImageOne.png";
-
 import blogImageOneCon from "./blogImageOneCon.png";
+import blogImageTwoCon from "./blogImageTwoCon.png";
+import blogImageThreeCon from "./blogImageThreeCon.png";
+import blogImageFourCon from "./blogImageFourCon.png";
+import blogImageFiveCon from "./blogImageFiveCon.png";
 
 export default {
     homeImage,
@@ -94,7 +78,6 @@ export default {
     TruffleIndlugenceImage,
     pacificFirecrackerImage,
     EternalEllEnchantment,
-    // backgroundGridImage,
     diamondIcon,
     backgroundImage,
     aboutImageOne,
@@ -116,4 +99,8 @@ export default {
     blogImageSix,
     reservationImageOne,
     blogImageOneCon,
+    blogImageTwoCon,
+    blogImageThreeCon,
+    blogImageFourCon,
+    blogImageFiveCon,
 }
