dc54a84eda5161696008c642b127caaf82ee7324
Kuba <272264@student.pwr.edu.pl>
2025-05-18 04:35:45 +0200

    Added responsivity to home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./home.css";
 import { PiInstagramLogoLight } from "react-icons/pi";
 import { PiFacebookLogo } from "react-icons/pi";
 import { PiTwitterLogoLight } from "react-icons/pi";
 import assets from '../../assets/assets';
 import HomeElement from '../../components/HomeElement/HomeElement';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className="home">
         <div className="homeContainer">
@@ -18,7 +30,7 @@ const Home = () => {
                     {/* Image Container */}
                     <div className="homeContainerLeftContainerImage">
                         <div className="homeContainerLeftContainerImageContainer">
-                            <img src={assets.homeImage} alt="" className="homeContainerLeftContainerImageContainerImage" />
+                            <img src={assets.homeImage} alt="Sushi restaurant main image" className="homeContainerLeftContainerImageContainerImage" />
                         </div>
                     </div>
 
@@ -38,25 +50,31 @@ const Home = () => {
                     <div className="homeContainerLeftContainerSocialMedia">
                         <div className="homeContainerLeftContainerSocialMediaContainer">
                             {/* Instagram */}
-                            <div className="homeContainerLeftContainerSocialMediaContainerInstagram">
-                                <div className="homeContainerLeftContainerSocialMediaContainerInstagramContainer">
-                                    <PiInstagramLogoLight className="homeContainerLeftContainerSocialMediaContainerInstagramContainerIcon" />
+                            <Link to="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                                <div className="homeContainerLeftContainerSocialMediaContainerInstagram">
+                                    <div className="homeContainerLeftContainerSocialMediaContainerInstagramContainer">
+                                        <PiInstagramLogoLight className="homeContainerLeftContainerSocialMediaContainerInstagramContainerIcon" />
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
 
                             {/* Facebook */}
-                            <div className="homeContainerLeftContainerSocialMediaContainerFacebook">
-                                <div className="homeContainerLeftContainerSocialMediaContainerFacebookContainer">
-                                    <PiFacebookLogo className="homeContainerLeftContainerSocialMediaContainerFacebookContainerIcon" />
+                            <Link to="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                                <div className="homeContainerLeftContainerSocialMediaContainerFacebook">
+                                    <div className="homeContainerLeftContainerSocialMediaContainerFacebookContainer">
+                                        <PiFacebookLogo className="homeContainerLeftContainerSocialMediaContainerFacebookContainerIcon" />
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
 
                             {/* Twitter */}
-                            <div className="homeContainerLeftContainerSocialMediaContainerTwitter">
-                                <div className="homeContainerLeftContainerSocialMediaContainerTwitterContainer">
-                                    <PiTwitterLogoLight className="homeContainerLeftContainerSocialMediaContainerTwitterContainerIcon" />
+                            <Link to="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                                <div className="homeContainerLeftContainerSocialMediaContainerTwitter">
+                                    <div className="homeContainerLeftContainerSocialMediaContainerTwitterContainer">
+                                        <PiTwitterLogoLight className="homeContainerLeftContainerSocialMediaContainerTwitterContainerIcon" />
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
                         </div>
                     </div>
 
